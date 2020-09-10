5cdf59ba267f22f7eabc92ff641670b6f029ada8
hamid814 <hamid331994@gmail.com>
2020-09-10 08:55:59 +0430

    dev

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`.magenta));
